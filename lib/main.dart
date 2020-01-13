847ec3a99449ac3da01746fe23fc7a54cce1e12a
john wanjema <jonwanjema@gmail.com>
2020-01-13 17:12:13 +0300

    added floating icon

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar:AppBar(
-      title:Text('Hello john'),
+      title:Text('Managers App'),
       centerTitle: true,
     ),
         body:Center(
           child: Text('i am john'),
         ),
       floatingActionButton:FloatingActionButton(
-        child: Text('Click Me')
+        child: Text('Click')
   ),
   )
   ,
