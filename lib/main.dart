ea0d680323e3cc75e6573a233f32a7b1290ab386
john wanjema <jonwanjema@gmail.com>
2020-01-13 19:22:58 +0300

    added hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar:AppBar(
-      title:Text('Managers App'),
-      centerTitle: true,
-      backgroundColor: Colors.red,
-    ),
-        body:Center(
-          child: Text(
-            'Hello john',
-            style: TextStyle(
-              fontSize: 20.0
-            ),
-          ),
-        ),
-      floatingActionButton:FloatingActionButton(
-        onPressed: (){},
-        backgroundColor: Colors.red,
-        child: Text('Click')
-  ),
-  )
-  ,
-));
+      home:Home()
+    ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Managers App'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text(
+          'Hello John wanjema',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.red,
+          child: Text('Click me')),
+    );
+  }
+}
