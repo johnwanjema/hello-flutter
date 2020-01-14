2baed39eedc5ef50e68f480caf0e882a209ba798
john wanjema <jonwanjema@gmail.com>
2020-01-14 21:04:15 +0300

    added columns and rows also changed alignment

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,43 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('App Bar'),
-          centerTitle: true,
-        ),
-        body:Column(
-            children: <Widget>[
-                Container(
-                    child: Text('first column'),
-                    color: Colors.redAccent,
-                ),
-                Container(
-                    child: Text('first column'),
-                    color: Colors.blue,
-                ),
-                Container(
-                    child: Text('first column'),
-                    color: Colors.green,
-                )
-            ],
-        ),
-        floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+      appBar: AppBar(
+        title: Text('App Bar'),
+        centerTitle: true,
+      ),
+      body: Column(
+        //vertical
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                    Text('hello'),
+                    Text('world')
+                ],
+            ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text('first column'),
+            color: Colors.redAccent,
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text('first column'),
+            color: Colors.blue,
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text('first column'),
+            color: Colors.green,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Text('click'),
-    ),
-
+      ),
     );
   }
 }
